b86350236262eff417fb401cbaeee6be176e56a8
Mohamed Anwer <151679950+MohamedAnwer0@users.noreply.github.com>
2025-08-10 21:20:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 // google auth provider
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const CLIENT_ID =
-    "232138544247-2mjvq8so7mmriiauq995kf6hqve7hsh6.apps.googleusercontent.com";
+const CLIENT_ID = ""; // Enter your client ID to get it. Go to console.cloud.google.com/
 // redux
 import { Provider } from "react-redux";
 import Store from "./Redux/Store.js";
